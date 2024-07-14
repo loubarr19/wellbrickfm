05ae2aa726d89205a41b68d07d9765c6d0bf4821
loubarr19 <wellbrickapparel@gmail.com>
2024-07-14 07:12:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 let isPlaying = false;
-const audio = document.getElementById('audio');
-const playPauseBtn = document.getElementById('playPauseBtn');
-const vinyl = document.getElementById('vinyl');
+let player;
 
-document.addEventListener('DOMContentLoaded', () => {
-    typeWriterEffect("Wellbrick FM presents the Gravy Train", "typewriter1", () => {
-        setTimeout(() => {
-            typeWriterEffect("Enjoy the ride!", "typewriter2", () => {
-                setTimeout(() => {
-                    typeWriterEffect("Let's get started!", "typewriter3");
-                }, 000);
-            });
-        }, 2000);
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '0',
+        width: '0',
+        videoId: 'R6_3OchvW_c',
+        events: {
+            'onReady': onPlayerReady
+        }
     });
-});
+}
 
-playPauseBtn.addEventListener('click', togglePlayPause);
+function onPlayerReady(event) {
+    // Initial play/pause button setup
+    document.getElementById('playPauseBtn').addEventListener('click', togglePlayPause);
+}
 
 function togglePlayPause() {
     if (isPlaying) {
-        audio.pause();
+        player.pauseVideo();
         playPauseBtn.textContent = 'Play';
         vinyl.style.animationPlayState = 'paused'; // Pause spinning
     } else {
-        audio.play();
+        player.playVideo();
         playPauseBtn.textContent = 'Pause';
         vinyl.style.animationPlayState = 'running'; // Resume spinning
     }
@@ -47,3 +47,15 @@ function typeWriterEffect(text, elementId, callback) {
 
     typeWriter();
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    typeWriterEffect("Wellbrick FM presents the Gravy Train", "typewriter1", () => {
+        setTimeout(() => {
+            typeWriterEffect("Enjoy the ride!", "typewriter2", () => {
+                setTimeout(() => {
+                    typeWriterEffect("Let's get started!", "typewriter3");
+                }, 500);
+            });
+        }, 1000);
+    });
+});
